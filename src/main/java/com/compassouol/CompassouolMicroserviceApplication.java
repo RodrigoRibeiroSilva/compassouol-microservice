e2e7362b7565152b8510907667e37389a72ad675
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2020-06-29 16:37:59 -0300

    Adicionadas as dependências do projeto

---
+++ src/main/java/com/compassouol/CompassouolMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.compassouol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassouolMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassouolMicroserviceApplication.class, args);
+	}
+
+}
